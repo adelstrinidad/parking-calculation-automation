f42a70a79c9f8610e4768475cfcec378d02bbace
Adelquis Trinidad <adelquis.trinidad@gmail.com>
2019-06-18 09:03:16 -0300

    add missing javadoc

--- src/main/java/com/dataprovider/DataProviderClass.java
+++ src/main/java/com/dataprovider/DataProviderClass.java
@@ -4,7 +4,16 @@ import com.csv.CSVDataProvider;
 
 import org.testng.annotations.DataProvider;
 
+/**
+ *
+ * @author Adelquis Trinidad
+ */
 public class DataProviderClass {
+
+    /**
+     *
+     * @return Returns the data to fill the data provider that calls this method
+     */
     @DataProvider(name="getDataFromCSV")
     public static Object[][] getDataFromCSV() {
         return CSVDataProvider.getCSVData("data.csv");
--- src/main/java/com/parkingcalculatortest/DriverFactory.java
+++ src/main/java/com/parkingcalculatortest/DriverFactory.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+/**
+ *
+ * @author Adelquis Trinidad
+ */
 public class DriverFactory {
     private  static WebDriver driver;
     private static WebDriverWait wait;
 
     private DriverFactory() {
-
+        //prevent instantiation
     }
 
+    /**
+     *
+     * @return driver Returns an instance of chrome
+     */
     public static WebDriver getChromeDriver() {
         if(driver == null) {
             System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
@@ -20,6 +28,10 @@ public class DriverFactory {
         return  driver;
     }
 
+    /**
+     *
+     * @return wait Return the implicit wait for chrome
+     */
     public static WebDriverWait getWebDriverWait() {
         if(wait == null) {
             wait = new WebDriverWait(getChromeDriver(), 5);
