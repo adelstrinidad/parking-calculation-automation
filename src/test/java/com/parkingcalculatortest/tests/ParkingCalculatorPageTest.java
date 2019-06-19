1b83da0da4e7b789c1173127ef548b15d0661c87
Adelquis Trinidad <adelquis.trinidad@gmail.com>
2019-06-19 10:27:54 -0300

    remove test and data provider with hardcoded values

--- src/test/java/com/parkingcalculatortest/tests/ParkingCalculatorPageTest.java
+++ src/test/java/com/parkingcalculatortest/tests/ParkingCalculatorPageTest.java
@@ -4,12 +4,9 @@ import com.dataprovider.DataProviderClass;
 import com.parkingcalculatortest.BaseTestClass;
 import com.parkingcalculatortest.ParkingCalculatorPage;
 
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Method;
-
-import static com.parkingcalculatortest.ParkingCalculatorPage.*;
+import static com.parkingcalculatortest.ParkingCalculatorPage.getParkingCalculationPage;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ParkingCalculatorPageTest extends BaseTestClass {
@@ -21,22 +18,8 @@ public class ParkingCalculatorPageTest extends BaseTestClass {
 
     ParkingCalculatorPage parkingCalculator = getParkingCalculationPage();
 
-    @DataProvider
-    public Object[][] dpParking(Method m) {
-        return new Object[][] {
-                { "STP", "11:50", "AM", "06/15/2019", "11:50", "PM", "06/15/2019", "$ 24.00", "(0 Days, 12 Hours, 0 Minutes)" },
-                { "STP", "09:30", "AM", "06/14/2019", "06:10", "PM", "06/15/2019", "$ 44.00", "(1 Days, 8 Hours, 40 Minutes)" },
-                { "STP", "11:50", "AM", "06/15/2019", "11:50", "PM", "06/14/2019", "ERROR! YOUR EXIT DATE OR TIME IS BEFORE YOUR ENTRY DATE OR TIME", "" },
-                { "LTG", "11:50", "AM", "06/15/2019", "11:50", "PM", "06/15/2019", "$ 12.00", "(0 Days, 12 Hours, 0 Minutes)" },
-                { "LTG", "09:30", "AM", "06/14/2019", "06:10", "PM", "06/15/2019", "$ 24.00", "(1 Days, 8 Hours, 40 Minutes)" },
-                { "LTG", "11:50", "AM", "06/15/2019", "11:50", "PM", "06/14/2019", "$ 0.00", "(-1 Days, 12 Hours, 0 Minutes)" }
-        };
-
-    }
-
     @Test(dataProvider = "getDataFromCSV", dataProviderClass = DataProviderClass.class)
     public void calculateParkingTestCSV(String lot, String startTime, String periodOfTimeEntry, String startDate, String endTime, String periodOfTimeExit,String endDate,  String cost, String timeSpent ) throws InterruptedException {
-        System.out.println(lot +" "+ startTime +" "+ periodOfTimeEntry +" "+ startDate +" "+ endTime  +" "+ periodOfTimeExit +" "+ endDate +" "+ cost +" "+ timeSpent);
 
         parkingCalculator.chooseLot(lot)
                 .chooseDateTime(ENTRY_TIME, startTime)
@@ -54,22 +37,4 @@ public class ParkingCalculatorPageTest extends BaseTestClass {
         }
     }
 
-    @Test(dataProvider = "dpParking")
-    public void calculateParkingTest(String lot, String startTime, String periodOfTimeEntry, String startDate, String endTime, String periodOfTimeExit,String endDate,  String cost, String timeSpent ) throws InterruptedException {
-        parkingCalculator.chooseLot(lot)
-                            .chooseDateTime(ENTRY_TIME, startTime)
-                            .choosePeriodTimeRB(ENTRY_TIME, periodOfTimeEntry)
-                            .setDate(ENTRY_DATE,startDate)
-                            .chooseDateTime(EXIT_TIME, endTime)
-                            .choosePeriodTimeRB(EXIT_TIME, periodOfTimeExit)
-                            .setDate(EXIT_DATE,endDate)
-                            .submit();
-
-        assertThat(cost).isEqualTo(parkingCalculator.calculateCost());
-        if(!cost.contains("ERROR")){
-            assertThat(timeSpent).isEqualTo(parkingCalculator.obtainTimeConsumed());
-        }
-
-
-    }
 }
